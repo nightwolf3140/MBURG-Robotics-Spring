e2d0b9f1bd32034ffa923f2a910f36861082fddf
WutangC <frederickwu0511@gmail.com>
2023-03-21 16:28:25 -0400

    added variables

--- LineTracking/RobotLibrary.h
+++ LineTracking/RobotLibrary.h
@@ -1,34 +1,49 @@
 int m =1; //Motor Vector
-//int uTurn = ?;
-//int pointTurn = ?;
+int uTurn = 340;
+int vpointTurn = 170;
 
 //throw functions below
-void move(char direction){
-	if(direction) == 'f'{
-		m=1;
-	}
-	if(direction) == 'b'{
-		m=1;
-	}
-}
-
-void uTurn(){
-	rsMotors();
-	//setMotorTarget(motorB, 
-
-}
-
 void rsMotors(){
 	resetMotorEncoder(motorB);
 	resetMotorEncoder(motorC);
 }
+
+void Go(char direction){
+	int speed=5*m;
+	if ((direction)=='f'){
+		rsMotors();
+		setMotorTarget(motorB,m,speed);
+		setMotorTarget(motorC,m,speed);
+	}
+	if ((direction)=='b'){
+		m=-1;
+		setMotorTarget(motorB,m,speed);
+		setMotorTarget(motorC,m,speed);
+	}
+}
+
+void uturn(){
+	rsMotors();
+	setMotorTarget(motorB,uTurn,50);
+	setMotorTarget(motorC,-uTurn,50);
+}
+
 void pointTurn(char direction){
-	
-	if(direction) == 'l'{ //Turn Left
+	if ((direction)=='l'){ //Turn Left
 		rsMotors();
+		setMotorTarget(motorB,vpointTurn,30);
+		setMotorTarget(motorC,-vpointTurn,30);
 	}
-	
-	if(direction) == 'r'{//Turn right
+
+	if ((direction)=='r'){//Turn right
 		rsMotors();
+		setMotorTarget(motorB,-vpointTurn,30);
+		setMotorTarget(motorC,vpointTurn,30);
 	}
+}
+
+void stp(){
+	rsMotors();
+	setMotorTarget(motorB,0,0);
+	setMotorTarget(motorC,0,0);
 }
